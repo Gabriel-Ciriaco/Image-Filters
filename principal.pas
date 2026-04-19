65c8c9dd381d403c8ac59a6641ff68dc71eb3caa
Gabriel Ciriaco de Carvalho <66225865+Gabriel-Ciriaco@users.noreply.github.com>
2026-04-18 22:39:58 -0300

    Filtro de Sobel adicionado
    Foi adicionado o Filtro de Sobel

--- principal.pas
+++ principal.pas
@@ -4,7 +4,7 @@ unit principal;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus,
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus, Math,
   Buttons, StdCtrls, ExtDlgs, Windows;
 
 type
@@ -13,17 +13,20 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    EditMagnitude: TEdit;
+    EditDirecao: TEdit;
     Image1: TImage;
     Image2: TImage;
     MainMenu1: TMainMenu;
-    MenuItem1: TMenuItem;
+    Arquivo: TMenuItem;
     MenuItem10: TMenuItem;
     MenuItem11: TMenuItem;
     MenuItem12: TMenuItem;
-    MenuItem2: TMenuItem;
-    MenuItem3: TMenuItem;
-    MenuItem4: TMenuItem;
-    MenuItem5: TMenuItem;
+    MenuItem13: TMenuItem;
+    Operacoes: TMenuItem;
+    Abrir: TMenuItem;
+    Salvar: TMenuItem;
+    Sair: TMenuItem;
     MenuItem6: TMenuItem;
     MenuItem7: TMenuItem;
     MenuItem8: TMenuItem;
@@ -31,11 +34,15 @@ type
     OpenDialog1: TOpenDialog;
     SavePictureDialog1: TSavePictureDialog;
     procedure Button1Click(Sender: TObject);
+    procedure EditMagnitudeChange(Sender: TObject);
     procedure Image1Click(Sender: TObject);
+    procedure Image2MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
+      );
     procedure MenuItem10Click(Sender: TObject);
     procedure MenuItem11Click(Sender: TObject);
     procedure MenuItem12Click(Sender: TObject);
-    procedure MenuItem3Click(Sender: TObject);
+    procedure MenuItem13Click(Sender: TObject);
+    procedure AbrirClick(Sender: TObject);
 
     // Operações
     procedure ConverterCinza;
@@ -48,13 +55,17 @@ type
     procedure BordaSobel;
     procedure Compressao;
     procedure Limiarizacao;
-    procedure MenuItem4Click(Sender: TObject);
-    procedure MenuItem5Click(Sender: TObject);
+    procedure SalvarClick(Sender: TObject);
+    procedure SairClick(Sender: TObject);
     procedure MenuItem6Click(Sender: TObject);
     procedure MenuItem7Click(Sender: TObject);
     procedure MenuItem8Click(Sender: TObject);
     procedure MenuItem9Click(Sender: TObject);
   private
+    SobelAtivo : Boolean;
+    magnitudes : array of array of Integer; // Array dinâmico com as magnitudes.
+    magDirecoes : array of array of Double;
+    procedure DesativarSobel;
 
   public
 
@@ -68,6 +79,13 @@ var
 
 implementation
 {$R *.lfm}
+
+procedure TForm1.DesativarSobel;
+begin
+  SobelAtivo := False;
+  EditMagnitude.Visible := False;
+  EditDirecao.Visible := False;
+end;
 
 // Operações
 procedure TForm1.ConverterCinza;
@@ -259,10 +277,148 @@ begin
 end;
 
 procedure TForm1.BordaSobel;
+var
+   i, j, SobelX, SobelY, minMag, maxMag, diffMag : Integer;
 begin
+  SetLength(magnitudes, ImgWidth, ImgHeight);
+  SetLength(magDirecoes, ImgWidth, ImgHeight);
+
+  for i := 0 to ImgWidth - 1 do
+   for j := 0 to ImgHeight - 1 do
+    begin
+      magnitudes[i, j] := 0;
+      magDirecoes[i, j] := 0;
+    end;
+
+
+  // Cálculo das bordas de sobel.
+  for i := 1 to ImgWidth - 2 do
+   for j := 1 to ImgHeight - 2 do
+    begin
+      SobelX :=  (-    ImE[i - 1, j - 1]  +     ImE[i - 1, j + 1]
+                  -2 * ImE[i, j - 1]      + 2 * ImE[i, j + 1]
+                  -    ImE[i + 1, j - 1]  +     ImE[i + 1, j + 1]) div 4;
+
+      SobelY :=  (-ImE[i - 1, j - 1] - 2 * ImE[i - 1, j] - ImE[i - 1, j + 1]
+                  +ImE[i + 1, j - 1] + 2 * ImE[i + 1, j] + ImE[i + 1, j + 1]) div 4;
+
+      magnitudes[i , j] := Round(Sqrt(SobelX * SobelX + SobelY * SobelY));
+
+      magDirecoes[i, j] := ArcTan2(SobelX, SobelY);
+    end;
+
+  // Normalização com mínimo e máximo da magnitude.
+  minMag := magnitudes[1, 1];
+  maxMag := magnitudes[1, 1];
+  for i := 1 to ImgWidth - 3 do
+   for j := 1 to ImgHeight - 3 do
+    begin
+      if minMag > magnitudes[i, j] then minMag := magnitudes[i, j];
+      if maxMag < magnitudes[i, j] then maxMag := magnitudes[i, j];
+    end;
+
+  diffMag := maxMag - minMag;
+
+  // Previnir divisão por zero.
+  if diffMag = 0 then diffMag := 1;
+
+  for i := 0 to ImgWidth - 3 do
+   for j := 0 to ImgHeight - 3 do
+    begin
+      ImS[i, j] := Round(255 * ((magnitudes[i, j] - minMag) / diffMag));
+
+      Image2.Canvas.Pixels[i, j] := RGB(ImS[i, j], ImS[i, j], ImS[i, j]);
+    end;
 
 end;
 
+
+procedure TForm1.SalvarClick(Sender: TObject);
+begin
+  SavePictureDialog1.DefaultExt := 'bmp';
+  if (SavePictureDialog1.Execute()) then Image1.Picture.SaveToFile(SavePictureDialog1.FileName);
+end;
+
+procedure TForm1.SairClick(Sender: TObject);
+begin
+  close();
+end;
+
+procedure TForm1.MenuItem6Click(Sender: TObject);
+begin
+  DesativarSobel;
+  ConverterCinza;
+end;
+
+procedure TForm1.MenuItem7Click(Sender: TObject);
+begin
+  DesativarSobel;
+  AdicionarRuido;
+end;
+
+procedure TForm1.MenuItem8Click(Sender: TObject);
+begin
+  DesativarSobel;
+  FiltroMedia;
+end;
+
+procedure TForm1.MenuItem9Click(Sender: TObject);
+begin
+  DesativarSobel;
+  FiltroMediana;
+end;
+
+procedure TForm1.Image1Click(Sender: TObject);
+begin
+end;
+
+procedure TForm1.Image2MouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  if not SobelAtivo then Exit;
+
+  // Verifica se o mouse está dentro dos limites calculados (evitar erros de índice)
+  if (X >= 1) and (X <= ImgWidth - 2) and (Y >= 1) and (Y <= ImgHeight - 2) then
+  begin
+    if (magnitudes <> nil) and (magDirecoes <> nil) then
+       begin
+         EditMagnitude.Text := 'Magnitude: ' + IntToStr(magnitudes[X, Y]);
+         EditDirecao.Text := 'Direção: ' + FloatToStrF(magDirecoes[X, Y], ffFixed, 7, 2);
+       end;
+  end
+  else
+  begin
+    EditMagnitude.Text := 'Magnitude: -';
+    EditDirecao.Text := 'Direção: -';
+  end;
+end;
+
+procedure TForm1.MenuItem10Click(Sender: TObject);
+begin
+  DesativarSobel;
+  Binarizacao;
+end;
+
+procedure TForm1.MenuItem11Click(Sender: TObject);
+begin
+  DesativarSobel;
+  FiltroLaplaciano;
+end;
+
+procedure TForm1.MenuItem12Click(Sender: TObject);
+begin
+  DesativarSobel;
+  EqualizacaoImagem;
+end;
+
+procedure TForm1.MenuItem13Click(Sender: TObject);
+begin
+  SobelAtivo := True;
+  EditMagnitude.Visible := True;
+  EditDirecao.Visible := True;
+
+  BordaSobel;
+end;
 procedure TForm1.Compressao;
 begin;
 
@@ -271,56 +427,6 @@ end;
 procedure TForm1.Limiarizacao;
 begin;
 
-end;
-
-procedure TForm1.MenuItem4Click(Sender: TObject);
-begin
-  SavePictureDialog1.DefaultExt := 'bmp';
-  if (SavePictureDialog1.Execute()) then Image1.Picture.SaveToFile(SavePictureDialog1.FileName);
-end;
-
-procedure TForm1.MenuItem5Click(Sender: TObject);
-begin
-  close();
-end;
-
-procedure TForm1.MenuItem6Click(Sender: TObject);
-begin
-  ConverterCinza;
-end;
-
-procedure TForm1.MenuItem7Click(Sender: TObject);
-begin
-  AdicionarRuido;
-end;
-
-procedure TForm1.MenuItem8Click(Sender: TObject);
-begin
-  FiltroMedia;
-end;
-
-procedure TForm1.MenuItem9Click(Sender: TObject);
-begin
-  FiltroMediana;
-end;
-
-procedure TForm1.Image1Click(Sender: TObject);
-begin
-end;
-
-procedure TForm1.MenuItem10Click(Sender: TObject);
-begin
-  Binarizacao;
-end;
-
-procedure TForm1.MenuItem11Click(Sender: TObject);
-begin
-  FiltroLaplaciano;
-end;
-
-procedure TForm1.MenuItem12Click(Sender: TObject);
-begin
-  EqualizacaoImagem;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -333,7 +439,12 @@ begin
           ImE[i, j] := ImS[i, j];
 end;
 
-procedure TForm1.MenuItem3Click(Sender: TObject);
+procedure TForm1.EditMagnitudeChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.AbrirClick(Sender: TObject);
 begin
   if (OpenDialog1.Execute()) then Image1.Picture.LoadFromFile(OpenDialog1.Filename);
   // Reseta a Imagem 2 se ela estiver preenchida.
@@ -359,6 +470,12 @@ begin
   // Posicionar o botao no meio entre as duas imagens.
   Button1.Left := (Image1.Left + Image1.Width) + Image1.Width div 4;
   Button1.Top := (Image1.Top + Image1.Height) div 2;
+
+  EditMagnitude.Left := (Image1.Left + Image1.Width) + Image1.Width div 6;
+  EditDirecao.Left := (Image1.Left + Image1.Width) + Image1.Width div 6 + EditMagnitude.Width;
+
+  EditMagnitude.Top := Image1.Top + Image1.Top div 2;
+  EditDirecao.Top := Image1.Top + Image1.Top div 2;
 
 end;
 
