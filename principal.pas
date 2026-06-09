86967acb8bdf446299aaf6b2e9f393cf822848ff
Carol-Nunes <cacanunesaraujo@gmail.com>
2026-06-09 17:21:46 -0300

    feat: implementa limiarização por Otsu
    Implementação do método de limiarização automática de Otsu.

--- principal.pas
+++ principal.pas
@@ -27,6 +27,7 @@ type
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     Laplaciano8: TMenuItem;
+    MenuItemLimiarizacaoOtsu: TMenuItem;
     MenuItemEqualizacaoHSL: TMenuItem;
     MenuItemPseudoCores: TMenuItem;
     MenuItemPontoMedio: TMenuItem;
@@ -69,9 +70,11 @@ type
     procedure Compressao(c: Float; y: Float);
     procedure Limiarizacao(t: Integer);
     procedure MenuItemEqualizacaoHSLClick(Sender: TObject);
+    procedure MenuItemLimiarizacaoOtsuClick(Sender: TObject);
     procedure MenuItemPseudoCoresClick(Sender: TObject);
     procedure PseudoCores;
     procedure EqualizacaoHSL;
+    procedure LimiarizacaoOtsu;
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
@@ -529,6 +532,12 @@ begin
   EqualizacaoHSL;
 end;
 
+procedure TForm1.MenuItemLimiarizacaoOtsuClick(Sender: TObject);
+begin
+  DesativarSobel;
+  LimiarizacaoOtsu;
+end;
+
 procedure TForm1.MenuItemPseudoCoresClick(Sender: TObject);
 begin
   DesativarSobel;
@@ -692,6 +701,81 @@ begin
 
       Image2.Canvas.Pixels[i, j] := RGB(R, G, B);
     end;
+end;
+
+procedure TForm1.LimiarizacaoOtsu;
+var
+  i, j, t, Threshold : Integer;
+  w0, uT, uT_classe, SigB2, SigT2, nf, nmin, n, x, y : Double;
+  pI : array[1..256] of Double;
+  ni : array[1..256] of Double;
+  jMin, kMax : Integer;
+begin
+  n := (ImgWidth - 4.0) * (ImgHeight - 4.0);
+
+  for i := 1 to 256 do
+    ni[i] := 0.0;
+
+  for i := 2 to ImgWidth - 3 do
+   for j := 2 to ImgHeight - 3 do
+    ni[ImE[i, j] + 1] := ni[ImE[i, j] + 1] + 1.0;
+
+  for i := 1 to 256 do
+    pI[i] := ni[i] / n; // pi : probabilidade de ocorrer o valor i na imagem
+
+  uT := 0.0; // uT : média global da imagem
+  for i := 1 to 256 do
+    uT := uT + i * pI[i];
+
+  SigT2 := 0.0; // SigmaT2 : variância global da imagem
+  for i := 1 to 256 do
+    SigT2 := SigT2 + (i - uT) * (i - uT) * pI[i];
+
+  jMin := -1;
+  kMax := -1;
+  for i := 1 to 256 do
+  begin
+    if (jMin < 0) and (pI[i] > 0.0) then jMin := i; // acha o menor com pi[i] != 0
+    if (pI[i] > 0.0) then kMax := i;                 // acha o maior com pi[i] != 0
+  end;
+
+  nmin := -1.0;
+  Threshold := jMin;
+
+  for t := jMin to kMax do
+  begin
+    uT_classe := 0.0;
+    for i := 1 to t do
+      uT_classe := uT_classe + i * pI[i];
+
+    w0 := 0.0;
+    for i := 1 to t do
+      w0 := w0 + pI[i];
+
+    x := (uT * w0 - uT_classe);
+    x := x * x;
+    y := w0 * (1.0 - w0);
+
+    if y > 0.0 then
+      x := x / y
+    else
+      x := 0.0;
+
+    SigB2 := x;
+
+    if SigT2 > 0.0 then
+      nf := SigB2 / SigT2
+    else
+      nf := 0.0;
+
+    if nf >= nmin then
+    begin
+      nmin := nf;
+      Threshold := t - 1;
+    end;
+  end;
+  ShowMessage('Threshold OTSU encontrado: ' + IntToStr(Threshold));
+  Limiarizacao(Threshold);
 end;
 
 procedure TForm1.MenuItem6Click(Sender: TObject);
